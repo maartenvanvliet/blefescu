46e958f2b2666d659d02123b5c63a952558b1b80
Maarten van Vliet <maartenvanvliet@gmail.com>
2017-12-10 18:55:42 +0100

    Make image dynamic and server configurable

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/discordapp/lilliput"
 )
+
+var addr string
+var baseURL string
 
 var EncodeOptions = map[string]map[int]int{
 	".jpeg": map[int]int{lilliput.JpegQuality: 85},
@@ -19,31 +23,56 @@ var EncodeOptions = map[string]map[int]int{
 }
 
 func imageHandler(w http.ResponseWriter, r *http.Request) {
-	var inputFilename = "test.jpg"
+	var imagePath = r.URL.Path
 	var outputWidth int
 	var outputHeight int
 	var stretch = true
 
-	fmt.Println("GET params were:", r.URL.Query())
-	outputWidth, err := strconv.Atoi(r.URL.Query().Get("w"))
-	if err != nil {
-		fmt.Printf("error parsing width, %s\n", err)
+	var client = &http.Client{
+		Timeout: time.Second * 10,
+	}
+	client.Transport = http.DefaultTransport
 
+	fmt.Println("Image path was:", baseURL+imagePath)
+	req, err := http.NewRequest("GET", baseURL+imagePath, nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		msg := fmt.Sprintf("invalid request URL: %v", err)
+		log.Print(msg)
+
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println("GET params were:", r.URL.Query())
+	outputWidth, err = strconv.Atoi(r.URL.Query().Get("w"))
+	if err != nil {
+		msg := fmt.Sprintf("error parsing width, %s\n", err)
+		log.Print(msg)
+
+		http.Error(w, msg, http.StatusBadRequest)
+		return
 	}
 
 	outputHeight, err = strconv.Atoi(r.URL.Query().Get("h"))
 	if err != nil {
-		fmt.Printf("error parsing height, %s\n", err)
+		msg := fmt.Sprintf("error parsing height, %s\n", err)
+		log.Print(msg)
 
+		http.Error(w, msg, http.StatusBadRequest)
+		return
 	}
 
-	inputBuf, err := ioutil.ReadFile(inputFilename)
+	inputBuf, err := ioutil.ReadAll(resp.Body)
 	decoder, err := lilliput.NewDecoder(inputBuf)
 	// this error reflects very basic checks,
 	// mostly just for the magic bytes of the file to match known image formats
 	if err != nil {
-		fmt.Printf("error decoding image, %s\n", err)
-		os.Exit(1)
+		msg := fmt.Sprintf("error decoding image, %s\n", err)
+		log.Print(msg)
+
+		http.Error(w, msg, http.StatusBadRequest)
+		return
 	}
 	defer decoder.Close()
 
@@ -51,10 +80,12 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 	// this error is much more comprehensive and reflects
 	// format errors
 	if err != nil {
-		fmt.Printf("error reading image header, %s\n", err)
-		os.Exit(1)
-	}
+		msg := fmt.Sprintf("error reading image header, %s\n", err)
+		log.Print(msg)
 
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
 	// print some basic info about the image
 	fmt.Printf("image type: %s\n", decoder.Description())
 	fmt.Printf("%dpx x %dpx\n", header.Width(), header.Height())
@@ -96,8 +127,11 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 	// resize and transcode image
 	outputImg, err = ops.Transform(decoder, opts, outputImg)
 	if err != nil {
-		fmt.Printf("error transforming image, %s\n", err)
-		os.Exit(1)
+		msg := fmt.Sprintf("error transforming image, %s\n", err)
+		log.Print(msg)
+
+		http.Error(w, msg, http.StatusBadRequest)
+		return
 	}
 
 	w.Header().Set("Content-Type", "image/jpeg")
@@ -111,6 +145,12 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	flag.StringVar(&addr, "addr", "localhost:8080", "TCP address to listen on")
+	flag.StringVar(&baseURL, "baseURL", "", "default base URL for relative remote URLs")
+	flag.Parse()
+
 	http.HandleFunc("/", imageHandler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Starting server: %s x %s\n", baseURL, addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
